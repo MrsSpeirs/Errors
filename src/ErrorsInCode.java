d14bf082977eea1f7713094d57fe5040e98cf746
MrsSpeirs <sspeirs@dasd.org>
2020-09-12 12:34:12 -0400

    Included buggy code for the project

---
+++ src/ErrorsInCode.java
@@ -0,0 +1,53 @@
+/*@author !!!!!!
+ * 9/12/2020, 12:18 PM */
+import java.util.Scanner;
+
+public class Error In Code {
+    public static void main(String[] args) {
+/**
+ * This program is for error checking.  Find the errors and fix them!
+ * When you have a working project with the following output - note the format:
+ *      The average of 100, 95 and 90 is: 95.0
+ *      The value of j is 3
+ *      The value of k is 1.5
+ *      100 / 10.0 equals 10.0
+ * Remember to change the name of the author and finish the comments
+ * to describe where, what and which type of error you found
+ * You must use variables for output and the value must be attained
+ * through the use of math operators and variables not just typing the output above
+ */
+
+
+                System.out.println("The average of 100, 95 and 90 is: ");
+                double average = 0;
+                average = 100 + 95 + 90 / 3;
+                System.out.println(average);
+
+                int i = 9, j = 0;
+                j = i / j;
+                System.out.println("The value of j is " + j);
+
+                int k = 9;
+                int l = 6;
+                k = k / l;
+                System.println("The value of k is  " + k);
+
+                int m = 100;
+                n = m/10;
+                System.out.println(100 / 10.0 equals + n);
+
+                //
+                //
+                //
+                //
+                //
+                //
+                //
+                //
+                //
+                //
+                //
+
+            }
+
+        }
